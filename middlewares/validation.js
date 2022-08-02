66e5a25794b5b50aad11ecf2fdb6aa62005cd9dd
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 17:15:34 -0300

    feat: creates age validation middleware

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -36,4 +36,21 @@ function validateName(req, res, next) {
   next();
 }
 
-module.exports = { validateLogin, validateName };
+function validateAge(req, res, next) {
+  const { age } = req.body;
+  if (!age) {
+    return res.status(400).json({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+
+  if (Number.isInteger(age) && age < 18) {
+    return res.status(400).json({
+      message: 'A pessoa palestrante deve ser maior de idade',
+    });
+  }
+
+  next();
+}
+
+module.exports = { validateLogin, validateName, validateAge };
