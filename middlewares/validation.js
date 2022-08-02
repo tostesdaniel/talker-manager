b6cfdabd52708a58e6957dbc7907124543df3604
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 17:09:03 -0300

    feat: creates login validation middleware

---
+++ middlewares/validation.js
@@ -0,0 +1,19 @@
+function validateLogin(req, res, next) {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
+module.exports = { validateLogin };
