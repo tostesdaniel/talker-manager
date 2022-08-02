a22e8c9e2d21408f245274dd48aa10f6a552b5ff
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 17:14:37 -0300

    feat: creates name validation middleware

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,17 +3,37 @@ function validateLogin(req, res, next) {
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
+
   const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
   if (!emailRegex.test(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
+
   if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
+
   if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();
 }
 
-module.exports = { validateLogin };
+function validateName(req, res, next) {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+
+  next();
+}
+
+module.exports = { validateLogin, validateName };
