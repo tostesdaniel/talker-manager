390c13c3a6450488fef52f551b489e837140b946
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 17:54:41 -0300

    feat: creates POST /talker route
    It verifies authorization, name, age, date and rate.
    On success returns new talker created.
    On fail returns an error.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const crypto = require('crypto');
 const fsUtils = require('./fs-utils');
-const { validateLogin } = require('./middlewares/validation');
+const {
+  validateLogin,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  validateRate,
+} = require('./middlewares/validation');
+const authMiddleware = require('./middlewares/authMiddleware');
 
 const app = express();
 const PORT = '3000';
@@ -46,6 +54,33 @@ app.post('/login', validateLogin, (_req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   return res.status(200).json({ token });
 });
+
+app.post(
+  '/talker',
+  authMiddleware,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  validateRate,
+  async (req, res) => {
+    const { name, age, talk } = req.body;
+    try {
+      const talker = await fsUtils.readFile();
+      const talkerExists = talker.some((t) => t.name === name);
+      if (talkerExists) {
+        return res.status(409).json({ message: 'Talker already exists' });
+      }
+      const lastTalkerId = talker.at(-1).id;
+      const newTalker = { id: lastTalkerId + 1, name, age, talk };
+      talker.push(newTalker);
+      await fsUtils.writeFile(talker);
+      return res.status(201).json(newTalker);
+    } catch (error) {
+      return res.status(500).end();
+    }
+  },
+);
 
 app.listen(PORT, () => {
   console.log('Online');
