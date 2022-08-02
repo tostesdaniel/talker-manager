1b09217a8120f54410a7a3c43af9710179b318ae
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 17:12:47 -0300

    feat: creates authorization validation middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,10 @@
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
