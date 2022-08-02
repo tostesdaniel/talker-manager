ad77e0a2a950df3b941ce951e56a292d83bdc12f
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-02 18:53:28 -0300

    feat: modifies  rate verification to garantee its a number

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -83,12 +83,12 @@ function validateWatchedAt(req, res, next) {
 function validateRate(req, res, next) {
   const { talk: { rate } } = req.body;
 
-  if (!rate) {
-    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  if ((Number.isInteger(rate) && Number(rate) < 1) || Number(rate) > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
-  if ((Number.isInteger(rate) && rate < 1) || rate > 5) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  if (!rate) {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
 
   next();
